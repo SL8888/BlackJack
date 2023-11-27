9e1f1c21835491d6ec8d7443a5e64a828da534b7
группы стационара <st@itstep.lan>
2023-11-27 21:45:14 +0300

    Initial commit

---
+++ src/main/java/Hand.java
@@ -0,0 +1,28 @@
+import java.util.List;
+
+public abstract class Hand {
+    protected List<Card> hand;
+
+    public abstract void play(Deck deck);
+
+    public void takeCard(Deck deck) {
+        hand.add(deck.giveCard());
+    }
+
+    public int getScore() {
+        int score = 0;
+        for (Card card : hand) {
+            score += score;
+        }
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        String s = null;
+        for (Card card : hand) {
+            s = s + card.toString();
+        }
+        return s;
+    }
+}
